5d8e54060af065ff686289479897392f686b3251
Madiyar <jenis.madiyar@gmail.com>
2022-12-09 15:41:44 +0600

    Initial Commit

---
+++ My hero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My hero
+//
+//  Created by Madik on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
